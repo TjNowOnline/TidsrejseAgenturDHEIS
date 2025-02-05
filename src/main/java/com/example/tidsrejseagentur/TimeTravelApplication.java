0bb59d4a4285d7b104a755a11538b5df805b9fdd
TjNowOnline <dmastek@hotmail.com>
2025-02-05 09:50:46 +0100

    menu

--- src/main/java/com/example/tidsrejseagentur/TimeTravelApplication.java
+++ src/main/java/com/example/tidsrejseagentur/TimeTravelApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Tidsrejse agen");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Tidsrejse");
         stage.setScene(scene);
         stage.show();
     }
